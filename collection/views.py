b191717488f2d854b328849bbffa3753beff68b2
iceviper1914 <mehiorob@gmail.com>
2018-11-27 13:06:29 -0500

    Created the Login and Registration

--- collection/views.py
+++ collection/views.py
@@ -7,5 +7,5 @@ def index(request):
     return render(request, 'index.html',{'books':books,})
 
 def book_detail(request, slug):
-    book= Book.objects.get(slug=slug)
+    book = Book.objects.get(slug=slug)
     return render(request, 'books/book_detail.html', { 'book': book,})
